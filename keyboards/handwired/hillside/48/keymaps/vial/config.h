a5b79fd1c6cf8d3edb6c5346136cbd12be24517c
Leo Lou <louyuhong@gmail.com>
2022-10-04 23:46:16 +0800

    Add hillside48

---
+++ keyboards/handwired/hillside/48/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+// Copyright 2022 Michael McCoyd (@mmccoyd)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3E, 0x98, 0xD7, 0x2B, 0x56, 0xC3, 0xFF, 0xB9}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 3 }
+
+// Uncomment if you want the right side to be master
+// #define MASTER_RIGHT
+#define EE_HANDS
+#define	RGBLIGHT_ANIMATIONS
